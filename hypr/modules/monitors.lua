1854279d0d44fff4ea85e1cf6323a178530cc67f
Jackson Clack <jacksonclack4@gmail.com>
2026-08-05 17:37:08 -0500

    Minor documentation changes and modularization testing  - Moved monitor configuration to monitors.lua file

---
+++ hypr/modules/monitors.lua
@@ -0,0 +1,3 @@
+-- https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({output = "DP-2"    , mode = "preferred" , position = "auto-right", scale = 1})
+hl.monitor({output = "HDMI-A-1", mode = "preferred" , position = "auto-left", scale = 1})
