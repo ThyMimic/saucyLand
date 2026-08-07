4c7db540bab864bf0424e2b08ce7ed71c5bebbdb
Jackson Clack <jacksonclack4@gmail.com>
2026-08-07 01:42:26 -0500

    Waybar - Updated subprocess pipeline data structure within equalizer.py. Script now averages audio ascii into 4 element arrays to be parsed later

--- waybar/equalizer.py
+++ waybar/equalizer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 import subprocess
 
+
+""" 
+Not too sure about this, pretty sure though that this executes cava since python is technically
+the first thing we really open in this entire data pipeline. The whole subprocess thing just makes
+cava technically the CHILD of the python program, like such: 
+
+Forgive me my internet overlords, this is my first time dealing with anything subprocess pipeline
+related
+   waybar       
+      equalizer.py  <-^ (averages data to send back to waybar)
+         cava > > >---| (sends data back to the script)
+"""
 cava = subprocess.Popen(
     ["cava"],
     stdout = subprocess.PIPE,
@@ -8,5 +20,25 @@ cava = subprocess.Popen(
     bufsize = 1
 )
 
+
+#This is basically saying for every line of output cava sends, do allat
 for line in cava.stdout:
-    print(line.rstrip())
+    values = line.strip().strip(";").split(";")
+
+    #Just checking to make sure everything in the original string was split up correctly
+    if len(values) != 8:
+        continue
+
+    #This forms the array of integers from the split string made earlier
+    values = [int(value) for value in values]
+
+    #THIS forms the array of averaged values, moving from an 8 element array to a 4 element array
+    four_bands = [
+        (values[0] + values[1]) / 2,
+        (values[2] + values[3]) / 2,
+        (values[4] + values[5]) / 2,
+        (values[6] + values[7]) / 2,
+    ]
+
+    #Epic print
+    print(four_bands)
